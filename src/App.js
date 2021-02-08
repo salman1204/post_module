1cd3272ea838a48a7fa97fbfe1e03db7c9cdc3bc
salman1204 <md.salman@alphasq.com>
2021-02-08 15:12:46 +0600

    get request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import useApiRequest from "./useApiRequest";
+import "./App.css";
 
 function App() {
+  const [url,setUrl] = useState('');
+  const {error, isLoaded, data} = useApiRequest(url);
+
+  const urlPara = () => {
+    setUrl ('https://jsonplaceholder.typicode.com/posts/1');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={()=> urlPara()}>Click Me</button>
+      <button onClick={()=> console.log(data)}>data</button>
     </div>
   );
 }
